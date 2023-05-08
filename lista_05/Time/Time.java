96ae111adeced985784c5561db327e319b4fa63a
lucasceu <lukasceu@gmail.com>
2023-05-08 19:15:52 -0300

    commit da lista 5

---
+++ lista_05/Time/Time.java
@@ -0,0 +1,211 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Time{
+    private int hora;
+    private int min;
+    private int seg;
+    private static int horaComp, minComp, segComp;
+
+    public Time(){
+        this(0);
+    }
+
+    public Time(int hour){
+        this(hour, 0);
+    }
+
+    public Time(int hour, int minute){
+        this(hour, minute, 0);
+    }
+
+    public Time(int hour, int minute, int second){
+        this.hora = hour;
+        this.min = minute;
+        this.seg = second;
+    }
+
+    public Time(Time t){
+        this.hora = t.getHour();
+        this.min = t.getMinute();
+        this.seg = t.getSecond();
+    }
+
+    public void addTime(int s){
+        if(s < 0){
+            System.out.printf("Invalid seconds!!\n");
+            System.exit(-1);
+        }
+        this.seg = this.seg + s;
+        if(this.seg >= 60){
+            this.min = this.min + this.seg / 60;
+            this.seg = this.seg % 60;
+            if(this.min >= 60){
+                this.hora = this.hora + this.min / 60;
+                this.min = this.min % 60;
+                if(this.hora >= 24){
+                    this.hora = this.hora % 24;
+                }
+            }
+        }
+    }
+
+    public void addTime(int m, int s){
+        if((m < 0) || (s < 0)){
+            System.out.println("Error in addTime, invalide minute!!");
+            System.exit(-1);
+        }
+        this.addTime(s);
+        this.min = this.min + m;
+        if(this.min >= 60){
+            this.hora = this.hora + this.min / 60;
+            this.min = this.min % 60;
+            if(this.hora >= 24){
+                this.hora = this.hora % 24;
+            }
+        }
+    }
+
+    public void addTime(int h, int m, int s){
+        if((h < 0) || (m < 0) || (s < 0)){
+            System.out.println("Error in addTime, invalide minute!!");
+            System.exit(-1);
+        }
+        this.addTime(m, s);
+        this.hora = this.hora + h;
+        if(this.hora >= 24){
+            this.hora = this.hora % 24;
+        }
+    }
+
+    public void addTime(Time obj){
+        this.addTime(obj.getHour(), obj.getMinute(), obj.getSecond());
+    }
+
+    public boolean isAm(){
+        if((this.hora < 12) && (this.hora > 0)){
+            return true;
+        }
+        return false;
+    }
+
+    public int cron(Time outraHora){
+        int hour = mode(outraHora.getHour() - this.hora);
+        int minute = mode(outraHora.getMinute() - this.min);
+        int second = mode(outraHora.getSecond() - this.seg);
+
+        return (hour * 3600) + (minute * 60) + (second);
+    }
+
+    private static int mode(int n){
+        if(n < 0){
+            return ((-1) * n);
+        }
+        return n;
+    }
+
+    public void addSeconds(int secs){
+        if(secs < 0){
+            System.out.printf("Invalid seconds!!\n");
+            System.exit(-1);
+        }
+        this.seg = this.seg + secs;
+        if(this.seg >= 60){
+            this.min = this.min + this.seg / 60;
+            this.seg = this.seg % 60;
+            if(this.min >= 60){
+                this.hora = this.hora + this.min / 60;
+                this.min = this.min % 60;
+                if(this.hora >= 24){
+                    this.hora = this.hora % 24;
+                }
+            }
+        }
+    }
+
+    public int getHour(){
+        return this.hora;
+    }
+
+    public int getMinute(){
+        return this.min;
+    }
+
+    public int getSecond(){
+        return this.seg;
+    }
+
+    private boolean validateTime(int hora, int min, int seg){
+        return validateHour(hora) && validateMinOrSec(min) && validateMinOrSec(seg);
+    }
+
+    private boolean validateHour(int hora){
+        if((hora>0) && (hora<25))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean validateMinOrSec(int param){
+        if((param>0) && (param<61))
+            return true;
+        else
+            return false;
+    }
+
+    public void setTime(int hora, int min, int seg){
+        if (validateTime(hora, min, seg)) {
+            this.hora = hora;
+            this.min = min;
+            this.seg = seg;
+        }
+        else{
+            this.hora = 0;
+            this.min = 0;
+            this.seg = 0;
+        }
+    }
+
+    public String getTime(){
+        String res = this.hora + ":" + this.min + ":" + this.seg;
+        return res;
+    }
+
+    public void prnTime(){
+        System.out.println(getTime());
+    }
+
+    public String toString(){
+        return hora + ":" + min + ":" + seg + "\n";
+    }
+
+    private static void setHoraComputador(){
+        GregorianCalendar horario = new GregorianCalendar();
+        horaComp = horario.get(Calendar.HOUR);
+        minComp = horario.get(Calendar.MINUTE);
+        segComp = horario.get(Calendar.SECOND);
+    }
+
+    public static int diffTime(Time t1, Time t2){
+        return (mode(t1.getHour() - t2.getHour()) * 60 * 60) + (mode(t1.getMinute() - t2.getMinute()) * 60) + mode(t1.getSecond() - t2.getSecond());
+    }
+
+    public static int diffTime(Time t){
+        setHoraComputador();
+        return (mode(t.getHour() - horaComp) * 60 * 60) + (mode(t.getMinute() - minComp) * 60) + mode(t.getSecond() - segComp);
+    }
+
+    public static boolean isAm(Time t){
+        if(t.getHour() < 12){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPm(Time t){
+        if(!isAm(t)){
+            return true;
+        }
+        return false;
+    }
+}
